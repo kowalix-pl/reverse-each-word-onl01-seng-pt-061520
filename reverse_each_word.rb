95944c931f0ea48a18001da1cd174b455a8ac4fd
kowalix007 L.K <kowalix007@gmail.com>
2020-06-19 07:15:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
-require 'pry'
+
 def reverse_each_word(sentence)
    string = []
    array = sentence.split(" ")
     # binding.pry
    string << array.collect {|word| word.reverse}
    return string.join(" ")
- 
 end
